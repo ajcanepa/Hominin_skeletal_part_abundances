877b99fef8df1fbab50b52c35b4d70976e3f1898
JonasGrabbe <grabbe.jonas@gmail.com>
2023-12-13 18:34:35 +0100

    first

---
+++ tidyPackage/skeleton.R
@@ -0,0 +1,12 @@
+#' Skeleton Data Frame
+#'
+#' A dataset containing the count of each skeletal part in a single human skeleton.
+#' @format A data frame with 32 rows and 2 variables:
+#' \describe{
+#'   \item{Elements}{The name of the skeletal element.}
+#'   \item{One_Skeleton}{The number of each element in one skeleton.}
+#' }
+"skeleton"
+
+
+## in package create with "devtools::document()" the Rd file. 
